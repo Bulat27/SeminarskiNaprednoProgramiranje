7e5007241d849409672b8dff28876b244b93d35c
Bulat27 <nbulat99@gmail.com>
2022-06-29 20:21:50 +0200

    Dodata main klasa.

---
+++ Server/src/main/java/rs/ac/bg/fon/nprog/server/main/Main.java
@@ -0,0 +1,14 @@
+package rs.ac.bg.fon.nprog.server.main;
+
+import rs.ac.bg.fon.nprog.server.view.coordinator.ViewCoordinator;
+
+/**
+ *
+ * @author Dragon
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ViewCoordinator.getInstance().openMainForm();
+    }
+}
