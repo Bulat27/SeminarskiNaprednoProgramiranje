b3fa54b84cc6e7f3eb27a10cf67ad09df487a2a3
Bulat27 <nbulat99@gmail.com>
2022-06-29 20:59:48 +0200

    Dodata Main klasa za pokretanje klijentske strane programa.

---
+++ Client/src/main/java/rs/ac/bg/fon/nprog/client/main/Main.java
@@ -0,0 +1,22 @@
+
+package rs.ac.bg.fon.nprog.client.main;
+
+import rs.ac.bg.fon.nprog.client.communication.Communication;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Dragon
+ */
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Communication.getInstance().connect();
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
